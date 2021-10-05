ac02ddd6aac935a47d91deee5ea3072d68392891
Nur Faizin <nur.faizin@accelbyte.net>
2021-10-05 21:26:07 +0700

    Add cache module.

---
+++ tests/test_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Copyright 2021 AccelByte Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for `iam_python_sdk.cache` module."""
+
+from iam_python_sdk.cache import Cache
+
+
+class TestCache:
+    def test_set_get_item(self):
+        cache = Cache(60, x=1)
+        cache['y'] = 2
+        cache.set('z', 3)
+        assert cache['x'] == 1
+        assert cache['y'] == 2
+        assert cache.get('z') == 3
+
+    def test_is_expired(self):
+        cache = Cache(60, x=1)
+        cache['y'] = 2
+        cache.set('z', 3, 30)
+        assert cache.is_expired('x') is False
+        assert cache.is_expired('y', 30) is False
+        assert cache.is_expired('z', 35) is True
