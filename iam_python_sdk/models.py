f6a89658ef5cd98bb98ffa588815c340aea1c981
Nur Faizin <nur.faizin@accelbyte.net>
2021-10-05 21:31:33 +0700

    Add model base class.

--- iam_python_sdk/models.py
+++ iam_python_sdk/models.py
@@ -14,20 +14,66 @@
 
 """Model module."""
 
-from typing import List
+from typing import Any, List, Set
+from datetime import datetime
+
+from .utils import decode_model
 
 
-class TokenResponse:
+class Model:
+    """Base model class."""
+    @classmethod
+    def loads(cls, data: Any) -> Any:
+        """Decode data to model
+
+        Args:
+            data (Any): data to decode
+
+        Returns:
+            Any: model object
+        """
+        return decode_model(data, cls())
+
+
+class Permission(Model):
+    """Holds information about the actions can be performed to the resource."""
+    Resource: str = ""
+    Action: int = -1
+    ScheduleAction: int = -1
+    CronSchedule: str = ""
+    RangeSchedule: List[str] = [""]
+
+
+class Role(Model):
+    """Hold info about a user role."""
+    RoleID: str = ""
+    RoleName: str = ""
+    Permissions: List[Permission] = [Permission()]
+
+
+class NamespaceRole(Model):
+    """Hold info about a namespace role."""
+    RoleID: str = ""
+    Namespace: str = ""
+
+
+class JWTBan(Model):
+    """Holds information about ban record in JWT."""
+    Ban: str = ""
+    EndDate: datetime = datetime.now()
+
+
+class TokenResponse(Model):
     """Token response class on successful token request."""
     AccessToken: str = ""
     RefreshToken: str = ""
     ExpiresIn: str = ""
     TokenType: str = ""
-    Roles: List[str] = []
-    AcceptedPolicyVersion: List[str] = []
-    NamespaceRoles: object = None  # NamespaceRole
-    Permissions: object = None  # Permission
-    Bans: object = None  # JWTBan
+    Roles: List[str] = [""]
+    AcceptedPolicyVersion: List[str] = [""]
+    NamespaceRoles: List[NamespaceRole] = [NamespaceRole()]
+    Permissions: List[Permission] = [Permission()]
+    Bans: List[JWTBan] = [JWTBan()]
     UserID: str = ""
     PlatformID: str = ""
     PlatformUserID: str = ""
@@ -37,37 +83,15 @@ class TokenResponse:
     IsComply: str = ""
 
 
-class Permission:
-    """Holds information about the actions can be performed to the resource."""
-    Resource: str = ""
-    Action: int = -1
-    ScheduleAction: int = -1
-    CronSchedule: str = ""
-    RangeSchedule: List[str] = []
-
-
-class Role:
-    """Hold info about a user role."""
-    RoleID: str = ""
-    RoleName: str = ""
-    Permission: object = None  # Permission
-
-
-class NamespaceRole:
-    """Hold info about a namespace role."""
-    RoleID: str = ""
-    Namespace: str = ""
-
-
-class JWTClaims:
+class JWTClaims(Model):
     """Holds data stored in a JWT access token with additional Justice Flags field."""
     Namespace: str = ""
     DisplayName: str = ""
-    Roles: List[str] = []
-    AcceptedPolicyVersion: List[str] = []
-    NamespaceRoles: List[object] = []  # NamespaceRole
-    Permissions: List[object] = []  # Permission
-    Bans: List[object] = []  # JWTBan
+    Roles: List[str] = [""]
+    AcceptedPolicyVersion: List[str] = [""]
+    NamespaceRoles: List[NamespaceRole] = [NamespaceRole()]
+    Permissions: List[Permission] = [Permission()]
+    Bans: List[JWTBan] = [JWTBan()]
     JusticeFlags: int = -1
     Scope: str = ""
     Country: str = ""
@@ -75,25 +99,19 @@ class JWTClaims:
     IsComply: bool = False
 
 
-class RevocationList:
-    """Contains revoked user and token."""
-    RevokedTokens: List[object] = []
-    RevokedUsers: List[object] = []  # UserRevocationListRecord
-
-
-class UserRevocationListRecord:
+class UserRevocationListRecord(Model):
     """Used to store revoked user data."""
     ID: str = ""
-    RevokedAt: object = None  # Timestamp
+    RevokedAt: datetime = datetime.now()
 
 
-class JWTBan:
-    """Holds information about ban record in JWT."""
-    Ban: str = ""
-    EndDate: object = None  # Timestamp
+class RevocationList(Model):
+    """Contains revoked user and token."""
+    RevokedTokens: Set[str] = {""}
+    RevokedUsers: List[UserRevocationListRecord] = [UserRevocationListRecord()]
 
 
-class ClientInformation:
+class ClientInformation(Model):
     """Holds client information."""
     ClientName: str = ""
     Namespace: str = ""
