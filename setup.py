2a1540ba364be2faa8cc130ee001954dedc5c431
Nur Faizin <nur.faizin@accelbyte.net>
2021-10-27 16:46:49 +0700

    Add explicit long_description_content_type.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords=['iam_python_sdk', 'accelbyte', 'iam'],
     name='iam-python-sdk',
