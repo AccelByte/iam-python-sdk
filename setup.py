e157a94b668e9d05535fdab4b97d120f3c117234
Nur Faizin <nur.faizin@accelbyte.io>
2021-11-01 08:17:46 +0700

    feat(ci): Add github actions and remove Travis-CI. (#12)
    * Add supoort for python 3.9
    
    * Add python 3.9 on travis.
    
    * Add travis script to deploy docs to gh-pages.
    
    * Add secure token to publish package to pypi.
    
    * Fixing travis script.
    
    * Add sphinx requirement.
    
    * Add all requirements.
    
    * Fixing before deploy script.
    
    * Fixing requirements.
    
    * Add github actions test.
    
    * Trigger github action test on push and PR.
    
    * Move github actions to the right location.
    
    * Add windows and macos for testing.
    
    * Fixing upgrade pip command.
    
    * Build for Python-3 only.
    
    * Update development status.
    
    * Add build & release to test pypi.
    
    * Add release to pypi.
    
    * Change filter tags.
    
    * Remove travis-CI.
    
    * Add upload artifact step.
    
    * Code cleaning.
    
    * Moving up artifact step.
    
    * Add commit sha to artifact name.
    
    * Add bump2version build sha.
    
    * Fixing bump2version build part.
    
    * Add build commit SHA to version for test.
    
    * Fixing bump2version build format.
    
    * Typo fixing.
    
    * Cut sha commit version to 8 char.
    
    * Fixing version name comply to PEP 440.
    
    * PEP 440
    
    * Fixing parse version.
    
    * Change local to RC format versioning.
    
    * Fixing RC versioning.
    
    * Fix github action RC versioning.
    
    * Publish to GH Pages when release published.
    
    * Add explicit long_description_content_type.
    
    * Downgrade sphinx.
    
    * Add sphinx-versioning to support multiversion docs
    
    * Fix sphinx-versioning branch filter.
    
    * Fixing package metadata.

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ requirements = ['Click>=7.0', 'backoff==1.11.1', 'httpx>=1.0.0b0', 'pyjwt[crypto
 test_requirements = ['pytest>=3', 'respx==0.18.0']
 
 setup(
-    author="AccelByte",
-    author_email='dev@accelbyte.net',
+    author="Analytics AccelByte",
+    author_email='justice-analytics-team@accelbyte.net',
     python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="AccelByte IAM Python SDK",
     entry_points={
@@ -37,13 +38,14 @@ setup(
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords=['iam_python_sdk', 'accelbyte', 'iam'],
     name='iam-python-sdk',
     packages=find_packages(include=['iam_python_sdk', 'iam_python_sdk.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/accelbyte/iam-python-sdk',
+    url='https://accelbyte.github.io/iam-python-sdk',
     version='0.8.0',
     zip_safe=False,
 )
