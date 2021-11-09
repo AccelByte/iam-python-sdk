5e41705c806a96bd96130b30318220ec607c3641
Nur Faizin <nur.faizin@accelbyte.net>
2021-11-09 13:05:34 +0700

    Bump version: 0.8.0 → 0.10.0

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://accelbyte.github.io/iam-python-sdk',
-    version='0.8.0',
+    version='0.10.0',
     zip_safe=False,
 )
