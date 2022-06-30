d2a6b42f246c73adc4f968bdbaaf25f07d9f3ad9
Nur <nur.faizin@accelbyte.net>
2022-06-30 14:42:50 +0700

    chore(logging): Reformat logging.

--- iam_python_sdk/log.py
+++ iam_python_sdk/log.py
@@ -14,5 +14,5 @@
 
 import logging
 
-logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s %(funcName)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-logger = logging.getLogger("[IAM-Python-SDK]")
+logging.basicConfig(format="%(asctime)s %(levelname)s [%(name)s:%(filename)s] %(funcName)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S %z")
+logger = logging.getLogger("IAM-Python-SDK")
