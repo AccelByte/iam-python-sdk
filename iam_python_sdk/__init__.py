77752c9e5d90ac67f418e628c456ee4e610265e1
Ghiffari <82567140+ghiffari-agsarya@users.noreply.github.com>
2025-01-09 04:03:48 +0000

    Bump version: 1.4.1 → 1.4.2

--- iam_python_sdk/__init__.py
+++ iam_python_sdk/__init__.py
@@ -18,7 +18,7 @@ from iam_python_sdk.config import Config
 
 __author__ = """Analytics AccelByte"""
 __email__ = 'justice-analytics-team@accelbyte.net'
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __all__ = [
     'NewDefaultClient',
     'NewAsyncClient',
